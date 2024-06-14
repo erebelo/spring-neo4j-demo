d85ded551ff2fc44fbdb8e7a50229bdff3e03b3c
erebelo <eduldrebelo@gmail.com>
2024-06-13 21:55:29 -0300

    Setting up the SSL to neo4j database

--- src/main/java/com/erebelo/springneo4jdemo/config/Neo4jConfiguration.java
+++ src/main/java/com/erebelo/springneo4jdemo/config/Neo4jConfiguration.java
@@ -2,6 +2,7 @@ package com.erebelo.springneo4jdemo.config;
 
 import lombok.RequiredArgsConstructor;
 import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.neo4j.config.AbstractNeo4jConfig;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @RequiredArgsConstructor
@@ -34,6 +38,12 @@ public class Neo4jConfiguration extends AbstractNeo4jConfig {
     @Value("${database.username:}")
     protected String dbUsername;
 
+    @Value("${database.ssl.enabled:true}")
+    private boolean sslEnabled;
+
+    @Value("${database.ssl.cert.path:}") // cert.pem file path
+    private String sslCertPath;
+
     // Heap memory security breach: do not use @Value annotation to get passwords
     private String getDbPassword() {
         return env.getProperty("database.password");
@@ -42,6 +52,19 @@ public class Neo4jConfiguration extends AbstractNeo4jConfig {
     @Bean
     public Driver driver() {
         var uri = String.format(CONNECTION_STRING_TEMPLATE, dbHost, dbPort, dbName);
-        return GraphDatabase.driver(uri, AuthTokens.basic(dbUsername, getDbPassword()));
+        return GraphDatabase.driver(uri, AuthTokens.basic(dbUsername, getDbPassword()), configureEncryptionOptions());
+    }
+
+    private Config configureEncryptionOptions() {
+        Config.ConfigBuilder configBuilder = Config.builder()
+                .withConnectionTimeout(30, TimeUnit.SECONDS)
+                .withTrustStrategy(Config.TrustStrategy.trustSystemCertificates());
+
+        if (sslEnabled && !sslCertPath.isEmpty()) {
+            var certFile = new File(sslCertPath);
+            configBuilder.withEncryption().withTrustStrategy(Config.TrustStrategy.trustCustomCertificateSignedBy(certFile));
+        }
+
+        return configBuilder.build();
     }
 }
