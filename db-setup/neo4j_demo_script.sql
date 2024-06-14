5af24342543a29aa2e29b1eef884e5328bdf054c
erebelo <eduldrebelo@gmail.com>
2024-06-14 12:55:34 -0300

    Updating neo4j script

--- db-setup/neo4j_demo_script.sql
+++ db-setup/neo4j_demo_script.sql
@@ -4,41 +4,41 @@
 CREATE CONSTRAINT FOR (u:User) REQUIRE u.username IS UNIQUE;
 
 -- Create User nodes
-CREATE (e:User {username: "@eduardo", name: "Eduardo"}),
-       (j:User {username: "@jose", name: "José"})
+CREATE (e:User {id: "abc45678-abcd-90ef-ghij-1234abcd5678", username: "@eduardo", name: "Eduardo"}),
+       (j:User {id: "def45678-abcd-90ef-ghij-1234abcd5678", username: "@jose", name: "José"})
 RETURN e, j;
 
 -----
 
--- Creates a Tweet node and associates it to a User node (@eduardo)
-CREATE (t:Tweet {title: "Tweet 1", content: "This is my first tweet!", likes: 0})
+-- Creates a Tweet node and associates it to a User node (@eduardo) through the POSTED relationship
+CREATE (t:Tweet {id: "ghi45678-abcd-90ef-ghij-1234abcd5670", title: "Tweet 1", content: "This is my first tweet!", likes: 0})
 WITH t
 MATCH (u:User {username: "@eduardo"})
 CREATE (u)-[:POSTED {createdAt: datetime()}]->(t)
-RETURN u, t
+RETURN u, t;
 
--- Creates a Tweet node and associates it to a User node (@eduardo)
-CREATE (t:Tweet {title: "Tweet 2", content: "This is my second tweet!", likes: 0})
+-- Creates a Tweet node and associates it to a User node (@eduardo) through the POSTED relationship
+CREATE (t:Tweet {id: "jkl45678-abcd-90ef-ghij-1234abcd5670", title: "Tweet 2", content: "This is my second tweet!", likes: 0})
 WITH t
 MATCH (u:User {username: "@eduardo"})
 CREATE (u)-[:POSTED {createdAt: datetime()}]->(t)
-RETURN u, t
+RETURN u, t;
 
--- Creates a Tweet node and associates it to a User node (@jose)
-CREATE (t:Tweet {title: "Tweet 1", content: "This is my first tweet!", likes: 0})
+-- Creates a Tweet node and associates it to a User node (@jose) through the POSTED relationship
+CREATE (t:Tweet {id: "mno45678-abcd-90ef-ghij-1234abcd5670", title: "Tweet 1", content: "This is my first tweet!", likes: 0})
 WITH t
 MATCH (u:User {username: "@jose"})
 CREATE (u)-[:POSTED {createdAt: datetime()}]->(t)
-RETURN u, t
+RETURN u, t;
 
 -----
 
--- Associates a User (@eduardo) to another User (@jose) and vice versa
+-- Associates a User (@eduardo) to another User (@jose) and vice versa through the FOLLOWS relationship
 MATCH (e:User {username: "@eduardo"})
 MATCH (j:User {username: "@jose"})
 CREATE (e)-[:FOLLOWS {sinceAt: datetime()}]->(j),
 	   (j)-[:FOLLOWS {sinceAt: datetime()}]->(e)
-RETURN e, j
+RETURN e, j;
 
 -----
 
@@ -52,9 +52,9 @@ DETACH DELETE u;
 
 -- Deletes a User node by username
 MATCH (u:User {username: "@eduardo"})
-DETACH DELETE u
+DETACH DELETE u;
 
--- Deletes one of the associations between two Users (@jose -> @eduardo)
+-- Deletes one of the FOLLOW relationships between two Users (@jose -> @eduardo)
 MATCH (j:User {username: "@jose"})-[f:FOLLOW]->(e:User {username: "@eduardo"})
 DELETE f
 RETURN j, e;
